d9a7dc1e6d370264a68ba29f16114a588e3a58f9
Thomas Dybdahl Ahle <thomas@ahle.dk>
2012-01-02 23:22:24 +0100

    Dir doesn't need add code, it's in point.

--- SokobanExam/src/uk/ax/ox/cs/sokobanexam/util/Dir.java
+++ SokobanExam/src/uk/ax/ox/cs/sokobanexam/util/Dir.java
@@ -1,26 +1,6 @@
 package uk.ax.ox.cs.sokobanexam.util;
 
-import uk.ac.ox.cs.sokobanexam.model.Point;
 
 public enum Dir {
-	NORTH (-1, 0),
-	EAST  ( 0, 1),
-	SOUTH ( 1, 0),
-	WEST  ( 0,-1);
-	
-	private int dy;
-	private int dx;
-	Dir (int dy, int dx) {
-		this.dy = dy;
-		this.dx = dx;
-	}
-	
-	/**
-	 * The method's name is very short, as methods on enums should be.
-	 * @param p
-	 * @return 
-	 */
-	public Point plus(Point p) {
-		return Point.at(p.x+dx, p.y+dy);
-	}
+	NORTH, EAST, SOUTH, WEST
 }
